e2e438f619c994c1251a0d29ba9ade7c4af4c9c3
milansav <StarzzDrak@seznam.cz>
2020-12-13 23:33:20 +0100

    Edit main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,24 @@ uint8_t parseCommand(std::string line)
 
 }
 
+std::vector<std::string> parseInput(std::string line)
+{
+    std::string temp = "";
+    std::vector<std::string> arguments;
+    //Split line into arguments
+    for(uint16_t i = 0; i < line.size(); i++)
+    {
+        if(isspace(line[i]))
+        {
+            arguments.push_back(temp);
+            temp = "";
+        }
+    }
+    arguments.push_back(temp);
+
+    return arguments;
+}
+
 enum EXPRESSION_TYPE
 {
     LITERAL,
@@ -31,6 +49,26 @@ int main()
     while(line != "END")
     {
         std::getline(std::cin, line);
+        std::vector<std::string> args = parseInput(line);
     }
     return 0;
+}
+
+void performAction(std::vector<std::string> arguments) //its bad but it does the job for now
+{
+    std::string first = arguments[0];
+
+    if(first == "LOAD");
+    else
+    if(first == "REMEMBER");
+    else
+    if(first == "PRINT");
+    else
+    if(first == "SUM");
+    else
+    if(first == "SUBTRACT");
+    else
+    if(first == "MULTIPLY");
+    else
+    if(first == "DIVIDE");    
 }
