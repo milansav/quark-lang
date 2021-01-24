408f44675749f0489d802e9b86dd779d9c14bdef
milansav <milan.savitskiy@email.cz>
2021-01-24 19:35:52 +0100

    parser updates

---
+++ parser/node.h
@@ -0,0 +1,57 @@
+#ifndef NODE_H
+#define NODE_H
+
+enum Type{
+    FUNCTION,
+    VARIABLE,
+    CONSTANT,
+    CONDITION,
+    LOOP,
+    RETURN,
+    ASSIGN,
+    BRANCH,
+    BIN_OP,
+    NO_OP
+};
+
+typedef struct function_node{
+
+} function_node;
+
+typedef struct variable_node{
+
+} variable_node;
+
+typedef struct constant_node{
+
+} constant_node;
+
+typedef struct condition_node{
+
+} condition_node;
+
+typedef struct loop_node{
+
+} loop_node;
+
+typedef struct return_node{
+
+} return_node;
+
+typedef struct assign_node{
+
+} assign_node;
+
+typedef struct branch_node {
+
+} branch_node;
+
+typedef struct bin_op_node{
+
+} bin_op_node;
+
+typedef struct no_op_node{
+
+} no_op_node;
+
+#endif
---
+++ parser/nodearr.h
@@ -0,0 +1,6 @@
+#ifndef NODEARR_H
+#define NODEARR_h
+
+
+
+#endif
